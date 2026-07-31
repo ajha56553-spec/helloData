591c4b610607071c2f657de808f995b9e109f097
ajha56553-spec <ajha56553@gmail.com>
2026-07-31 09:54:01 +0530

    Changes Done

--- helloMain.py
+++ helloMain.py
@@ -1,11 +1,15 @@
-
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, World!"
+    return "Hello Databricks App"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    import os
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ["DATABRICKS_APP_PORT"])
+    )
+``
